eea6b4c02c6391ce284600f18ae704592c3beeac
Mohanad-Ramadan <mohanadramadan.m1@gmail.com>
2024-06-26 17:05:11 +0300

    Initial Commit

---
+++ MasteringFirebase/MasteringFirebaseApp.swift
@@ -0,0 +1,31 @@
+//
+//  MasteringFirebaseApp.swift
+//  MasteringFirebase
+//
+//  Created by Mohanad Ramdan on 26/06/2024.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct MasteringFirebaseApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+
